27531fc733479c49ef27bd494116c2b3f876b586
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-11-12 16:13:26 +0800

    recommend获取数据

--- express.js
+++ express.js
@@ -1,8 +1,26 @@
 import express from 'express'
+import axios from 'axios'
+
 const app = express()
 
+app.get('/api/getRecommends',async(req,res)=>{
+    let {code,data} = await axios.get('https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg')
+    if(code) { console.log('/api/getRecommend Error')}
+    else if(data){
+        res.json(data)
+    }
+})
 
+app.all("*", function (req, res, next) {
 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+
+    next();
+});
 app.use(express.static('./dist'))
 export default app.listen(8300,function (err) {
     if (err) {
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,70 +1,122 @@
 <template>
-  <div>recommend</div>
+  <div class="recommend" ref="recommend">
+    <div class="slide-wrapper">
+          <div :autoPlay=true>
+            <div v-if="recommends.length" v-for="(item, index) in recommends" :key ="index">
+              <a :href="item.linkUrl">
+                <!-- better-scroll 与 fastclick冲突 添加needsclick fastclick会识别 -->
+                <img class="needsclick" @load="loadImg" :src="item.picUrl">
+              </a>
+            </div>
+          </div>
+        </div>
+    <!-- <router-view></router-view> -->
+  </div>
 </template>
 
 <script>
-export default{
-  
-}
+import jsonp from "common/js/jsonp";
+export default {
+  data() {
+    return {
+      recommends: [],
+      songList: []
+    };
+  },
+  async mounted() {
+    let {data} = await jsonp(
+      "https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg",
+      {},
+      {
+        timeout: 3000,
+        param: 'jsonpCallback'
+      }
+    );
+    console.log(data);
+    this.recommends = data.slider;
+    this.songList = data.songList;
+  },
+  methods: {}
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-  @import '~common/stylus/variable'
-  .recommend
-    position fixed
-    width 100%
-    top 88px
-    bottom 0
-    // flex-direction column
-    .recommend-content
-      height 100%
-      overflow hidden
-      .recommend-list
-        .list-title
-          line-height 44px
-          font-size $font-size-medium
-          color $color-theme
-          text-align center
-        .song-list
-          .song-item
+@import '~common/stylus/variable'
+
+.recommend {
+  position fixed
+  width 100%
+  top 88px
+  bottom 0
+
+  // flex-direction column
+  .recommend-content {
+    height 100%
+    overflow hidden
+
+    .recommend-list {
+      .list-title {
+        line-height 44px
+        font-size $font-size-medium
+        color $color-theme
+        text-align center
+      }
+
+      .song-list {
+        .song-item {
+          display flex
+          // padding 10px 0px
+          // margin 0px 20px
+          padding 0 20px 20px 20px
+          box-sizing border-box
+          // flex属性 在交叉轴上如何对齐
+          align-items center
+          // overflow hidden
+          font-size 0
+
+          .icon {
+            flex 0 0 60px
+            width 60px
+            padding-right 20px
+            vertical-align top
+            // img
+            // height 60px
+            // width 60px
+          }
+
+          .text {
+            flex 1
             display flex
-            // padding 10px 0px
-            // margin 0px 20px
-            padding 0 20px 20px 20px
-            box-sizing border-box
-            // flex属性 在交叉轴上如何对齐
-            align-items center
-            // overflow hidden
-            font-size 0
-            .icon
-              flex 0 0 60px
-              width 60px
-              padding-right 20px
-              vertical-align top
-              // img
-              //   height 60px
-              //   width 60px
-            .text
-              flex 1
-              display flex
-              flex-direction column
-              justify-content center
+            flex-direction column
+            justify-content center
+            overflow hidden
+            line-height 20px
+            font-size $font-size-medium
+
+            // margin-left 20px
+            // vertical-align top
+            .name {
+              white-space nowrap
+              text-overflow ellipsis
               overflow hidden
-              line-height 20px
-              font-size $font-size-medium
-              // margin-left 20px
-              // vertical-align top
-              .name
-                white-space nowrap
-                text-overflow ellipsis
-                overflow hidden
-                margin-bottom 20px
-                color $color-text
-              .desc
-                color $color-text-d
-      .loading-wrapper
-        position absolute
-        width 100%
-        top 50%
-        transform translateY(-50%)
+              margin-bottom 20px
+              color $color-text
+            }
+
+            .desc {
+              color $color-text-d
+            }
+          }
+        }
+      }
+    }
+
+    .loading-wrapper {
+      position absolute
+      width 100%
+      top 50%
+      transform translateY(-50%)
+    }
+  }
+}
 </style>
